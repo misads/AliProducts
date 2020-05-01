9129a0fd608b7bc40d9e8009f9d1854062be4476
misads <523131316@qq.com>
2020-05-02 00:08:42 +0800

    data argumentation

--- dataloader/products.py
+++ dataloader/products.py
@@ -1,3 +1,4 @@
+# encoding=utf-8
 import pdb
 
 import torchvision.transforms.functional as F
@@ -5,12 +6,69 @@ import os
 from PIL import Image
 import torch.utils.data.dataset as dataset
 from torchvision import transforms
+import torchvision.transforms as transforms
+
+
 import random
 import numpy as np
 import cv2
 
 
 from torch_template import torch_utils
+
+
+def data_augmentation(img, scale, degree, jitter, hue, saturation, val):
+    """
+        degree: 在(-degree, degree) 之间旋转
+        jitter: 随机放大(1, 1+jitter)倍
+        hue: 色调 (img-hue, img+hue)
+        saturation: 饱和度 (img-1/saturation, img+1/saturation)
+        val: 亮度 (img-1/val, img+1/val)
+    """
+    if degree != 0:
+        random_rotate = transforms.RandomRotation(degree)
+        img = random_rotate(img)
+
+    max_ex = int(scale * jitter)
+
+    ex_height = random.randint(0, max_ex)
+    ex_width = random.randint(0, max_ex)
+    img = F.resize(img, (scale + ex_height, scale + ex_width))  # 随机放大0.3
+
+    img = F.crop(img, ex_height, ex_width, scale, scale)  # crop回原来的尺寸
+
+    def rand_scale(s):
+        scale = np.random.uniform(1, s)
+        if np.random.randint(2):  # 增加或减少
+            return scale
+        return 1. / scale
+
+    dhue = np.random.uniform(-hue, hue)
+    dsat = rand_scale(saturation)
+    dexp = rand_scale(val)
+
+    img = img.convert('HSV')
+    cs = list(img.split())
+    cs[1] = cs[1].point(lambda i: i * dsat)
+    cs[2] = cs[2].point(lambda i: i * dexp)
+
+    def change_hue(x):
+        nonlocal dhue
+        x += dhue * 255
+        if x > 255:
+            x -= 255
+        if x < 0:
+            x += 255
+        return x
+
+    cs[0] = cs[0].point(change_hue)
+    img = Image.merge(img.mode, tuple(cs))
+
+    img = img.convert('RGB')
+    # constrain_image(im)
+
+    return img
+
 
 class TrainValDataset(dataset.Dataset):
     """ImageDataset for training.
@@ -71,9 +129,9 @@ class TrainValDataset(dataset.Dataset):
         if self.scale:
             input = F.resize(input, (self.scale, self.scale))
 
-        r = random.randint(0, 7)
-        if self.aug and r != 7:
-            input = input.transpose(self.trans_dict[r])
+        if self.aug:
+            input = data_augmentation(input, scale=self.scale, degree=0, jitter=0.3, hue=0.1, saturation=1.5, val=1.5)
+            #input = input.transpose(self.trans_dict[r])
 
         if self.norm:
             input = F.normalize(F.to_tensor(input), mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
@@ -160,11 +218,11 @@ def preview_dataset(dataset, path='path'):
 
 if __name__ == '__main__':
 
-    dataset = TrainValDataset('../train.txt', aug=False)
+    dataset = TrainValDataset('../datasets/train.txt', scale=320, aug=True)
     preview_dataset(dataset)
 
-    dataset = TestDataset('../test.txt')
-    preview_dataset(dataset)
+    # dataset = TestDataset('../test.txt')
+    # preview_dataset(dataset)
 
     # for i, data in enumerate(train_dataset):
     #     input, label, file_name = data
