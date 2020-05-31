c2ac954c1352736e0b5799b559d2698a8ec6736a
misads <523131316@qq.com>
2020-05-31 22:22:53 +0800

    fix minor

--- help.py
+++ help.py
@@ -31,10 +31,10 @@ Resume Training:
     python train.py --tag your_tag --model ResNeSt101 -b 24 --load checkpoints/{%s}/9_Model.pt """ % checkpoints_choices + """--resume
 
 Eval:
-    python eval.py --tag your_tag2 --model ResNeSt101 -b 24 --load checkpoints/{%s}/9_Model.pt """ % checkpoints_choices + """
+    python eval.py --tag your_tag2 --model ResNeSt101 -b 48 --load checkpoints/{%s}/9_Model.pt """ % checkpoints_choices + """
 
 Generate Submission:
-    python submit.py --model ResNeSt101 --load checkpoints/my_ckpt.pt --gpu 0'
+    python submit.py --model ResNeSt101 --load checkpoints/my_ckpt.pt -b 48 --gpu 0'
 
 See Running Log:
     cat logs/{%s}/log.txt""" % log_choices + """
