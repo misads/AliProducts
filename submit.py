f900bcdb3d53d2838436c7bfa6799d474941889c
misads <523131316@qq.com>
2020-05-31 12:10:28 +0800

    fix minor

--- submit.py
+++ submit.py
@@ -22,6 +22,7 @@ def parse_args():
     # to load correctly
     parser.add_argument('--optimizer', choices=['adam', 'sgd', 'radam', 'lookahead', 'ranger'], default='ranger')
     parser.add_argument('--scheduler', choices=['cos', 'step', 'exp', 'cyclic', 'lambda', 'None'], default='cos')
+    parser.add_argument('--epochs', '--max_epoch', type=int, default=10, help='epochs to train')
     parser.add_argument('--lr', type=float, default=0.0001, help='initial learning rate for adam')
 
     # batch size
